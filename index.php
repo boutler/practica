e8679a6e9040b6da8c27f778937f6fb0aa050e88
Alex <Alex@Alex-PC.home>
2013-05-30 20:42:03 +0200

    Versio inicial Del Proyecte

--- index.php
+++ index.php
@@ -1,17 +1,6 @@
 <?php
 
 /*
- * Funcio per fer una suma
+ * Versió inicial del proyecte
  */
-
-if (isset($_GET['a']) && isset($_GET['b'])){
- 
-    echo suma($_GET['a'],$_GET['b']);
-    
-    function suma($a,$b)  {  
-        $c=$a+$b;   
-            return $c;
-    }   
-}   
-
 ?>
