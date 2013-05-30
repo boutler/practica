ad9104fec709ac559288e5b62e44d5421014fde4
Alex <Alex@Alex-PC.home>
2013-05-30 20:27:21 +0200

    Versio inicial Del Proyecte

--- index.php
+++ index.php
@@ -3,4 +3,13 @@
 /*
  * Versió inicial del projecte
  */
+
+if (isset($_GET['a']) $$ isset($_GET['b'])){
+    
+    function suma($a,$b)    
+        $c=$a+$b;   
+            return $c;
+    }   
+}
+
 ?>
