ff99210ee5007d198574a00d8a32c97d852b63f1
Alex <Alex@Alex-PC.home>
2013-05-30 22:07:54 +0200

    Les tres funcions. mult, suma y resta

--- index.php
+++ index.php
@@ -11,11 +11,20 @@ function suma($a,$b){
 
     echo suma(3,5);
 
-    
+    //MULT
 function mult($a,$b){
     
     return $a*$b;
 }
 
     echo mult(3, 3);
+    
+    // RESTA
+ function resta($a,$b){
+    
+    return $a-$b;
+}
+
+    echo resta(3, 3);   
+    
 ?>
